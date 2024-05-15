92bd1aef72785818d6c4b9735b96ef3d7c45b121
yuta.fujioka <user081.dounyu@techacademy.jp>
2024-05-15 15:48:44 +0900

    index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -34,30 +32,39 @@ public class NewServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         //response.getWriter().append("Served at: ").append(request.getContextPath());
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
 
-        //Messageのインスタンスを生成
-        Message m = new Message();
+//        EntityManager em = DBUtil.createEntityManager();
+//        em.getTransaction().begin();
+//
+//        //Messageのインスタンスを生成
+//        Message m = new Message();
+//
+//        String title = "taro";
+//        m.setTitle(title);
+//
+//        String content = "hello";
+//        m.setContent(content);
+//
+//        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+//        m.setCreated_at(currentTime);
+//        m.setUpdated_at(currentTime);
+//
+//        //データベースに保存
+//        em.persist(m);
+//        em.getTransaction().commit();
+//
+//        //自動採番されたIDの値の表示
+//        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+//        em.close();
 
-        String title = "taro";
-        m.setTitle(title);
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        String content = "hello";
-        m.setContent(content);
+        //おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
 
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
-
-        //データベースに保存
-        em.persist(m);
-        em.getTransaction().commit();
-
-        //自動採番されたIDの値の表示
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-        em.close();
-
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
     }
 
 }
